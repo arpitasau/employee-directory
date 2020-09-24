527f76fb38b2a7e65b1cc338cffd38609e369893
Arpita K Sau <arpitakaran@gmail.com>
2020-09-24 15:13:47 -0500

    Updates

---
+++ precache-manifest.ae1406c59071d7b410d45a27b3b06436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f70885d57cc5ee9f0b21bafa4f6e9b",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "130256711174b5c0830b",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "69730f7e770fb0e5f1af",
+    "url": "/employee-directory/static/css/main.7d765ed4.chunk.css"
+  },
+  {
+    "revision": "130256711174b5c0830b",
+    "url": "/employee-directory/static/js/2.5626d89e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.5626d89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69730f7e770fb0e5f1af",
+    "url": "/employee-directory/static/js/main.eea67504.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
